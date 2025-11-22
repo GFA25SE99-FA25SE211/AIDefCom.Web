1eae5ad69bedbd1c22613567d77641007bc8b748
hiTech <minhtqse184278@fpt.edu.vn>
2025-11-22 18:34:19 +0700

    Set output mode to standalone in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  output: "standalone",
   // Bỏ devIndicators vì API cũ, Next 16 sẽ báo warning hoặc crash khi dùng boolean
   // Nếu muốn tắt loading indicator có thể dùng:
   // devIndicators: { buildActivity: false }
