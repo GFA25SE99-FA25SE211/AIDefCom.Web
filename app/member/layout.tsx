351fead477ad0281b1b8a9d0c04fe5d51570f8d7
Tuan An <ancntse171335@fpt.edu.vn>
2025-12-02 14:38:29 +0700

    Fix enroll check for Member and Secretary

--- app/member/layout.tsx
+++ app/member/layout.tsx
@@ -1,14 +1,17 @@
 import type { ReactNode } from "react";
 import MemberSidebar from "./groups-to-grade/components/Sidebar";
+import VoiceGuard from "@/components/auth/VoiceGuard";
 
 export default function MemberLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-[#F3F6FB]">
-      {/* Sidebar cố định */}
-      <MemberSidebar />
+    <VoiceGuard>
+      <div className="flex min-h-screen bg-[#F3F6FB]">
+        {/* Sidebar cố định */}
+        <MemberSidebar />
 
-      {/* Main content */}
-      <main className="flex-1 p-8">{children}</main>
-    </div>
+        {/* Main content */}
+        <main className="flex-1 p-8">{children}</main>
+      </div>
+    </VoiceGuard>
   );
 }
